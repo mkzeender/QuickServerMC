29a157eae8ee08eec959d39411a03180c80780eb
mkzeender <mkzeender@gmail.com>
2020-09-04 09:14:41 -0700

    Delete QuickServer.bat

--- QuickServer.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mkdir %appdata%\.QuickServer\bin
-cd %appdata%\.QuickServer\bin
-If NOT Exist Autohotkey.exe (
-curl -z QuickServer.ahk -o QuickServer.ahk https://raw.githubusercontent.com/mkzeender/QuickServerMC/master/QuickServer.ahk
-curl -z Autohotkey.zip -o Autohotkey.zip https://www.autohotkey.com/download/ahk.zip
-tar -x -f Autohotkey.zip
-)
-start "" Autohotkey.exe QuickServer.ahk
